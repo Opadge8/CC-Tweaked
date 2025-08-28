e5a0391ed6c58cb1a83729af7ddd5fde096b3a59
Opadge8 <81783545+Opadge8@users.noreply.github.com>
2025-08-28 21:22:06 +0000

    1

--- init.lua
+++ init.lua
@@ -115,8 +115,8 @@ local function home()
 
     local function drawMenu()
         setTheme()
-        box(1,1,w,h,"PersonalOS — "..(cfg.user or "player"))
-        center(3, "Use ↑/↓ or touch screen, Enter/Click to open, Q to quit to shell")
+        box(1,1,w,h,"Start Menu")
+        center(3, "Click to launch app")
         for i,app in ipairs(apps) do
             term.setCursorPos(4, appStartY + i - 1)
             if i == sel then term.setTextColor(theme.acc) else term.setTextColor(theme.fg) end
