88a65c159f6f301ceef311327bd280c3a28165e3
Opadge8 <81783545+Opadge8@users.noreply.github.com>
2025-08-28 20:39:58 +0000

    1

---
+++ config.lua
@@ -0,0 +1,14 @@
+local cfg = {}
+
+-- Theme
+cfg.theme = {
+    bg = colors.black,
+    fg = colors.white,
+    acc = colors.lightBlue,
+    warn = colors.red
+}
+
+-- User
+cfg.user = "player"
+
+return cfg
