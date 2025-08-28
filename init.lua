7682007cb98c8759378c2f40d766e3289ade3584
Opadge8 <81783545+Opadge8@users.noreply.github.com>
2025-08-28 22:24:31 +0000

    1

--- init.lua
+++ init.lua
@@ -94,7 +94,7 @@ end
 local function drawTime()
     term.setCursorPos(1, 1)
     term.write(textutils.formatTime(os.time()))
-    term.setCursorPos(25, 1)
+    term.setCursorPos(24, 1)
     term.write(textutils.formatTime(os.time("local")-4))
 end
 
